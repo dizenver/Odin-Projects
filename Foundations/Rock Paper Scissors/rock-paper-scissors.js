37da5fed19813b60a7beb449929d717e27d6f9db
Denver Martinez <denver.martinez@gmail.com>
2022-04-20 00:08:15 -0500

    Add all win/lose combinations and a anti-cheat fallback

--- Foundations/Rock Paper Scissors/rock-paper-scissors.js
+++ Foundations/Rock Paper Scissors/rock-paper-scissors.js
@@ -22,13 +22,19 @@ function playRound () {
   if (playerSelection === computerSelection) {
       console.log("Tie Game");
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    console.log ("You win the round");
+    console.log ("You win, Rock Beats Scissors!");
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log ("You win the round");
+    console.log ("You win, Paper Beats Rock");
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    console.log ("You win the round");
+    console.log ("You win, Scissors beats Paper!");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log ("You lose, Paper beats Rock!");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log ("You lose, Scissors beats Paper!");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log ("You lose, Rock beats Scissors!");
   } else {
-    console.log ("I lose");
+    console.log ("No Cheating, you lose.");
   }
 }
 
