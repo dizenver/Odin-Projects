afcedb14a093f159aab6a1b955aef9c34e95c21a
Denver Martinez <denver.martinez@gmail.com>
2022-04-19 22:47:48 -0500

    Step 1 Pseudocode

---
+++ Foundations/Rock Paper Scissors/rock-paper-scissors.js
@@ -0,0 +1,9 @@
+console.log("Hello World")
+
+//`prompt()` user for input (Rock, Paper or Scissors)
+//	convert to lower case and verify it is one of the three (case-sensitive answers)
+//Generate `computerSelection` variable using `computerPlay` function.
+
+//Push both player and computer selections through `playRound` function
+
+//Using `loop` call `playRound` function for a 5 round game and keep score.
