8ef97853264b7754f91030982f50b97d88164734
Denver Martinez <denver.martinez@gmail.com>
2022-04-19 23:16:43 -0500

    Add prompt for player choice and function for computer choice

--- Foundations/Rock Paper Scissors/rock-paper-scissors.js
+++ Foundations/Rock Paper Scissors/rock-paper-scissors.js
@@ -1,9 +1,25 @@
 console.log("Hello World")
 
 //`prompt()` user for input (Rock, Paper or Scissors)
+
+let mySelection = prompt("Choose your weapon. Rock, Paper or Scissors", "");
+
 //	convert to lower case and verify it is one of the three (case-sensitive answers)
+let playerSelection = mySelection.toLowerCase();
+
+console.log(playerSelection);
+
 //Generate `computerSelection` variable using `computerPlay` function.
+function computerPlay() {
+    let weaponChoice = ['rock', 'paper', 'scissors']
+    return weaponChoice[Math.random()*weaponChoice.length | 0];
+}
+let computerSelection = computerPlay();
+console.log(computerSelection);
 
 //Push both player and computer selections through `playRound` function
+function playRound (playerSelection, computerPlay) {
+  
+}
 
 //Using `loop` call `playRound` function for a 5 round game and keep score.
